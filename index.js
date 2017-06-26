38d9a01a54d9b12f13f5b4375c7a907f6115db22
Ake Tangkananond <iamake@gmail.com>
2017-06-26 23:46:16 +0700

    Loading packages

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello NodeJS');
+var moment = require('moment-timezone');
+
+var now = moment();
+var timezones = [
+  'Europe/London',
+  'Asia/Bangkok',
+  'Asia/Tokyo',
+];
+
+timezones.forEach(function (timezone) {
+  console.log(now.tz(timezone).format('lll z'));
+});
